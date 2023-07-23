fe69de37a003f86835bb847a9a88eb15449a2274
Davies Aniefiok <daviesaniefiok32@gmail.com>
2023-07-23 22:50:52 +0000

    added task 1

--- 0x00-pagination/1-simple_pagination.py
+++ 0x00-pagination/1-simple_pagination.py
@@ -25,10 +25,10 @@ class Server:
     """Server class to paginate a database of popular baby names.
     """
     DATA_FILE = "Popular_Baby_Names.csv"
-    
+
     def __init__(self):
         self.__dataset = None
-        
+
     def dataset(self) -> List[List]:
         """Cached dataset"""
         if self.__dataset is None:
@@ -37,7 +37,7 @@ class Server:
                 dataset = [row for row in reader]
             self.__dataset = dataset[1:]
         return self.__dataset
-    
+
     def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
         """return the proper page of the data"""
         assert type(page) is int and page > 0
