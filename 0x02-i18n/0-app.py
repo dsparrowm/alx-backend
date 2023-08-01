d71efde3e1fdd033b076891ac645055e6e60231f
Davies Aniefiok <daviesaniefiok32@gmail.com>
2023-08-01 11:08:53 +0000

    added task 0

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -9,8 +9,8 @@ app = Flask(__name__)
 @app.route('/')
 def hello():
     """a simple function that renders a html page"""
-    return rwnder_template('0-index.html')
+    return render_template('0-index.html')
 
 
 if __name__ == "__main__":
-    app.run(port="5000", host="0.0.0.0")
+    app.run(port="5000", host="0.0.0.0", debug=True)
