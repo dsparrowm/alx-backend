871e6b233effc62214839bba937141a6d78554db
Davies Aniefiok <daviesaniefiok32@gmail.com>
2023-08-01 10:54:03 +0000

    added task 1

---
+++ 0x02-i18n/1-app.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+"""A basic flask application"""
+from flask import Flask, render_template
+from flask_babel import Babel
+
+
+class Config:
+    """contains config for my flask app"""
+    LANGUAGES = ['en', 'fr']
+    BABEL_DEFAULT_LOCALE = 'en'
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
+
+
+#configure thw flask app
+app = Flask(__name__)
+app.config.from_object(Config)
+
+
+@app.route('/')
+def hello():
+    """renders a simple index file"""
+    return render_template('1-index.html')
+
+
+if __name__ == "__main__":
+    app.run(port="5000", host="0.0.0.0")
